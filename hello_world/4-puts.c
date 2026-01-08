167d95d1b65122c237f9906ba28849fe2dca4e41
Angel-ingabire <a.ingabire2@alustudent.com>
2026-01-08 08:50:52 -0800

    4-puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Prints a message using puts
- *
- * Return: 0 (Success)
- */
+* main - Prints a message using puts
+*
+* Return: 0 (Success)
+*/
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return (0);
+puts("Programming is like building a multilingual puzzle");
+return (0);
 }
 
