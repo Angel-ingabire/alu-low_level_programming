e1905a63a76883ef7a2340e165de5e67819614fb
Angel-ingabire <a.ingabire2@alustudent.com>
2026-01-24 05:13:52 -0800

    task 8 task 9 task 10 and task 11

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -9,4 +9,8 @@ void print_alphabet_x10(void);
 int print_sign(int n);
 int _abs(int n);
 int print_last_digit(int n);
+void jack_bauer(void);          /* Task 8 */
+void times_table(void);         /* Task 9 */
+int add(int a, int b);          /* Task 10 */
+void print_to_98(int n);        /* Task 11 */
 #endif
